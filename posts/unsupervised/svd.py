6699ef451a80619ae7166139e529d2c628460735
Kent Canonigo <kcanonigo@middlebury.edu>
2023-04-10 11:23:10 -0400

    Finished intro of blog; need to implement svd

---
+++ posts/unsupervised/svd.py
@@ -0,0 +1,13 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+class SVD:
+    
+    def svd_reconstruct(img, k):
+        '''
+        Input: Image to reconstruct and number of k singular values to use
+        Output: Reconstructs image from its singular value decomposition
+        '''
+        
+    
+    
