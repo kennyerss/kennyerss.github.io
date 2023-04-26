cdecd114c7c4610ab0a6fb055144bbbc75474606
Kent Canonigo <kcanonigo@middlebury.edu>
2023-04-26 01:36:51 -0400

    Finished Dr. Gebru blog post

--- posts/unsupervised/svd.py
+++ posts/unsupervised/svd.py
@@ -8,6 +8,26 @@ class SVD:
         Input: Image to reconstruct and number of k singular values to use
         Output: Reconstructs image from its singular value decomposition
         '''
+        # From lecture notes
+        U, sigma, V = np.linalg.svd(img)
         
+        # Create the D matrix in the SVD
+        D = np.zeros_like(img,dtype=float) # matrix of zeros of same shape as img
+        # Singular values on the main diagonal
+        D[:min(img.shape),:min(img.shape)] = np.diag(sigma)      
+        
+        # Approximate using the first k columns of U, D, and V
+        U_ = U[:, :k]
+        D_ = D[:k, :k]
+        V_ = V[:k, :]
+        
+        # Reconstruct our image 
+        img_ = U_ @ D_ @ V_
+        return img_
     
     
+    def svd_experiment(img, k):
+        '''
+        Input: Image to 
+        Output:
+        '''
